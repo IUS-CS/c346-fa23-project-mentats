798e7af6c08b6aab1de8bc0cde01fbf8a23b717b
Nyx <nbcomplete@pm.me>
2023-08-31 03:58:52 -0400

    Added the base spring boot application boilerplate

---
+++ statego-backend/src/test/java/com/mentats/stategobackend/StategoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentats.stategobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StategoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
