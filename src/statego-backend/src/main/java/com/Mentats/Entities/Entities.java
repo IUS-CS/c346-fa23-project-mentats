c9df8514cd71ae2f13bf891b1e12ed648a481fd9
browjor <browjor@iu.edu>
2023-09-18 11:27:07 -0400

    Started creating an entity class for sessions recorded by the user. This mostly involves figuring out how to handle a simple date.

---
+++ src/statego-backend/src/main/java/com/Mentats/Entities/Entities.java
@@ -0,0 +1,27 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Session {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  private String email;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  
+}
