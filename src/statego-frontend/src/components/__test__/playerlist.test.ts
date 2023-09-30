c70d558f039c8077b460b4e809215780adf8ea2b
Tyler Popson <nbcomplete@pm.me>
2023-09-30 17:44:17 -0400

    added more thurough test for player list component

--- src/statego-frontend/src/components/__test__/playerlist.test.ts
+++ src/statego-frontend/src/components/__test__/playerlist.test.ts
@@ -5,5 +5,16 @@ import PlayerList from "../PlayerList.vue";
 
 test("mount component", async () => {
 	expect(PlayerList).toBeTruthy();
-	const wrapper = mount(PlayerList)
+	const wrapper = mount(PlayerList, {
+		props: {
+			winner: "Player 1",
+			players: ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5']
+		}
+	});
+	expect(wrapper.text()).toContain("Player 1");
+	expect(wrapper.text()).toContain("Player 2");
+	expect(wrapper.text()).toContain("Player 3");
+	expect(wrapper.text()).toContain("Player 4");
+	expect(wrapper.text()).toContain("Player 5");
+	
 });
