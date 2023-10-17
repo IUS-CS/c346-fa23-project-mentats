888cdc963409200df4b930438b3fb6fc9f257534
Tyler Popson <tylerpopson@gmail.com>
2023-10-17 19:49:38 -0400

    positioned the navbar on the left of the screen

--- src/statego-frontend/src/App.vue
+++ src/statego-frontend/src/App.vue
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-   <header class="h-15 fixed top-0 flex w-full bg-slate-700">
+   <header class="fixed left-0 flex h-full w-20 bg-slate-700">
       <NavBar />
    </header>
    <div class="flex h-screen items-center justify-center bg-gray-300 pt-16">
--- src/statego-frontend/src/components/NavBar.vue
+++ src/statego-frontend/src/components/NavBar.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-   <nav class="flex h-full w-full items-center justify-center">
+   <nav class="flex h-full w-full flex-col items-center">
       <RouterLink class="p-5 text-white hover:bg-slate-900" to="/">Home</RouterLink>
       <RouterLink class="p-5 text-white hover:bg-slate-900" to="/about">About</RouterLink>
       <RouterLink class="p-5 text-white hover:bg-slate-900" to="/logsession">Log Session</RouterLink>
