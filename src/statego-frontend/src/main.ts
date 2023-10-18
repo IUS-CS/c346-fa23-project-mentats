9db4384c35b64656652db1c87d2c83a6cda04131
Tyler Popson <tylerpopson@gmail.com>
2023-10-17 22:24:12 -0400

    added icons and changed colors of nav bar

--- src/statego-frontend/src/main.ts
+++ src/statego-frontend/src/main.ts
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { LaHomeSolid, LaArrowRightSolid, LaUserSolid, LaInfoCircleSolid, LaPlusSolid } from 'oh-vue-icons/icons';
+
+addIcons(LaHomeSolid, LaArrowRightSolid, LaUserSolid, LaInfoCircleSolid, LaPlusSolid);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component('v-icon', OhVueIcon);
 
 app.mount('#app');
