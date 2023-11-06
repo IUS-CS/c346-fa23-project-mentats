77ef4c3821d649980bd1d824987f97407b3e720d
Tyler Popson <nbcomplete@pm.me>
2023-11-06 15:19:42 -0500

    created the basic landing page

--- src/statego-frontend/src/views/LandingView.vue
+++ src/statego-frontend/src/views/LandingView.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMouseInElement } from '@vueuse/core';
+import { ref } from 'vue';
+import type { RouterLink } from 'vue-router';
+
+const target = ref(null);
+const logo1 = ref<HTMLElement>();
+const logo2 = ref<HTMLElement>();
+const logo3 = ref<HTMLElement>();
+const logo4 = ref<HTMLElement>();
+
+const mouseRel = useMouseInElement(target);
+
+</script>
 
 <template>
-   <div class="flex h-full w-full flex-col items-center justify-center">
-      <p class="font-Nunito-Sans">Hello This is the Statego Landing page</p>
-   </div>
+   <div class="flex w-full flex-col lg:flex-row justify-center items-center">
+      <div ref="target" class="flex w-4/5 lg:w-1/3 lg:p-10 items-center justify-center">
+         <img ref="logo1" src="/statego-logo-v2.svg" alt="Statego Logo" class="fixed">
+         <img ref="logo2" src="/statego-logo-v2.svg" alt="Statego Logo" class="fixed">
+         <img ref="logo3" src="/statego-logo-v2.svg" alt="Statego Logo" class="fixed">
+         <img ref="logo4" src="/statego-logo-v2.svg" alt="Statego Logo" class="fixed">
+      </div>
+      <div class="pt-10 lg:pt-0 flex flex-col justify-center items-center w-4/5 lg:w-2/3">
+         <h1 class="font-Nunito-Sans text-5xl md:text-9xl">STATEGO</h1>
+         <p class="font-Nunito-Sans text-2xl">A board game statistics tracker and anaysis tool</p>
+         <div>
+            <RouterLink to="/signup">Signup</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+         </div>
+      </div>
+   </div>   
 </template>
